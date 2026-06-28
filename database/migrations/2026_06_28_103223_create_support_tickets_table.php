d529d8c7652a669c74f15bfd666e40c4a4777040
infoscert <eshanokpe@gmail.com>
2026-06-28 11:51:35 +0100

    feat: implement 2026_06_28_103223_create_support_tickets_table

--- database/migrations/2026_06_28_103223_create_support_tickets_table.php
+++ database/migrations/2026_06_28_103223_create_support_tickets_table.php
@@ -8,21 +8,23 @@ return new class extends Migration
 {
     public function up()
     {
-        Schema::create('support_tickets', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email');
-            $table->string('phone')->nullable();
-            $table->string('subject');
-            $table->text('message');
-            $table->string('status')->default('pending');
-            $table->boolean('is_read')->default(false);
-            $table->timestamps();
-            
-            $table->index('email');
-            $table->index('status');
-            $table->index('is_read');
-        });
+         if (!Schema::hasTable('support_tickets')) {
+            Schema::create('support_tickets', function (Blueprint $table) {
+                $table->id();
+                $table->string('name');
+                $table->string('email');
+                $table->string('phone')->nullable();
+                $table->string('subject');
+                $table->text('message');
+                $table->string('status')->default('pending');
+                $table->boolean('is_read')->default(false);
+                $table->timestamps();
+                
+                $table->index('email');
+                $table->index('status');
+                $table->index('is_read');
+            });
+         }
     }
 
     public function down()
